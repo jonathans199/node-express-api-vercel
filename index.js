27898b72b60b1ab34bc23ff2ee28807d38dad755
jonathans199 <jso1919@gmail.com>
2022-07-06 22:04:29 -0400

    first setup commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
